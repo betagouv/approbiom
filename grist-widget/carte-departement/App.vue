1b91458d3591e54addb3a0dd2d8bf56040afa0a7
Clémence Fernandez <clemencefrz@gmail.com>
2026-05-26 13:56:49 +0200

    add console log to track row on record

--- grist-widget/carte-departement/App.vue
+++ grist-widget/carte-departement/App.vue
@@ -7,8 +7,15 @@ grist.ready({
   allowSelectBy: true,
 });
 
-grist.onRecord((data) => {
-  console.log("data test", data);
+grist.onRecord((row) => {
+  try {
+    if (row === null) {
+      throw new Error("(No data - not on row - please add or select a row)");
+    }
+    console.log("GOT...", JSON.stringify(row));
+  } catch (err) {
+    throw new Error(`error : ${err}`);
+  }
 });
 </script>
 
